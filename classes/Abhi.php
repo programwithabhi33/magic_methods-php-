ec677304d94a272c25a948013db00070a67f92a3
abhi <programwithabhi@gmail.com>
2022-09-26 03:29:33 -0700

    Understand the get in the php

--- classes/Abhi.php
+++ classes/Abhi.php
@@ -8,7 +8,9 @@ class Abhi{
     // The __get function allow the object to accees the private variable in the class this is read only (public variables are read and write only but private variables are not accessible but you can access it with the help of __get function but read only)
     // It takes one argument of the variable name
     public function __get($name){
-        echo "Getting abhi variable $name";
+        echo "Getting abhi variable ".$name;
+        echo "Getting abhi variable ".$this->$name;
+        // echo $this->$name;
     }
 }
 
--- main.php
+++ main.php
@@ -1,11 +1,11 @@
 <?php
 
-// include './classes/Abhi.php';
+include './classes/Abhi.php';
 // include './classes/Main.php';
 
-function __autoload($className){
-    include_once "./classes/$className.php";
-}
+// function __autoload($className){
+//     include_once "./classes/$className.php";
+// }
 
 $obj = new Abhi();
 
